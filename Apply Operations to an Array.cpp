6faa0d00d3d6a6fde0420cf33d4c38ca416cdeff
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-03-01 12:55:32 +0530

    Create Apply Operations to an Array.cpp

---
+++ Apply Operations to an Array.cpp
@@ -0,0 +1,23 @@
+//level:medium
+//link:https://leetcode.com/problems/apply-operations-to-an-array/description/?envType=daily-question&envId=2025-03-01
+//solution
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        
+        int n = nums.size();
+        int index=0;
+        while(index<n-1)
+        {
+            if(nums[index]==nums[index+1])
+            {
+                nums[index]=2*nums[index];
+                nums[index+1]=0;
+            }
+            index++;
+        }
+        stable_partition(nums.begin(), nums.end(), [](int n) { 
+                                       return n != 0; });
+        return nums;
+    }
+};
