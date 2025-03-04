8a681611056f3049450481980bbf058aa73bcf18
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-03-04 19:53:02 +0530

    Create Check if Number is a Sum of Powers of Three.cpp

---
+++ Check if Number is a Sum of Powers of Three.cpp
@@ -0,0 +1,19 @@
+//level: Medium
+//link: https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/
+//solution
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+       while(n>0)
+       {
+        if(n%3==2)
+        {
+            return false;
+        }
+        n=n/3;
+       }
+       return true;
+       
+    }
+};
