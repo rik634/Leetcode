27d6aa756f300c7e6ef07b349b63c2742000dd0d
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-06-28 11:19:10 +0530

    Create Assign Cookies.cpp

---
+++ Assign Cookies.cpp
@@ -0,0 +1,28 @@
+// Level: Easy
+// Link: https://leetcode.com/problems/assign-cookies/description/
+// Solution
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        int n = g.size();
+        int m = s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0,r=0;
+        int count=0;
+        while(l<n && r<m)
+        {
+            if(s[r]>=g[l])
+            {
+                count++;
+                l++;
+            }
+            r++;
+        }
+        return count;
+    }
+};
+
+// Time complexity: M*logM + N*logN + M
+// Space Complexity: O(1)
