8e1bb5387d9841f856e4402acefdf4d710c6932d
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-08-03 12:28:30 +0530

    Update Maximum fruits harvested after at most k steps.cpp

--- Maximum fruits harvested after at most k steps.cpp
+++ Maximum fruits harvested after at most k steps.cpp
@@ -37,3 +37,46 @@ public:
 };
 // time complexity = O(n+klogn)
 // space complexity: O(n)
+
+
+
+class Solution {
+public:
+    int step(vector<vector<int>>& fruits, int &startPos, int left, int right)
+    {
+        if(fruits[right][0]<=startPos)
+        {
+            return startPos-fruits[left][0];
+        }
+        else if(fruits[left][0]>=startPos)
+        {
+            return fruits[right][0]-startPos;
+        }
+        else
+        {
+            return min(abs(startPos-fruits[right][0]),abs(startPos-fruits[left][0])) + fruits[right][0]-fruits[left][0];
+        }
+    }
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        
+        int left=0;
+        int right=0;
+        int n = fruits.size();
+        int sum=0;
+        int total=0;
+        while(right<n)
+        {
+            sum+=fruits[right][1];
+            while(left<=right && step(fruits,startPos, left, right)>k)
+            {
+                sum-=fruits[left][1];
+                left++;
+            }
+            total=max(total,sum);
+            right++;
+        }
+        return total;
+    }
+};
+// time complecity: O(2n)
+// space complecity: O(1)
