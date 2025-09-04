7a85099bf7f8aac26047b2cd786a464b546bfd2b
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-09-04 23:05:46 +0530

    Create Find closest person.cpp

---
+++ Find closest person.cpp
@@ -0,0 +1,18 @@
+// level: easy
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+
+        int a = abs(z-x);
+        int b = abs(z-y);
+        if(a<b)
+        {
+            return 1;
+        }
+        else if(b<a)
+        {
+            return 2;
+        }
+        return 0;
+    }
+};
