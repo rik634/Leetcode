af2d19603796e3b65d82242b7282fdc4472a208c
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-10-14 23:43:49 +0530

    Create Adjacent Increasing Subarray Detection I.cpp

---
+++ Adjacent Increasing Subarray Detection I.cpp
@@ -0,0 +1,28 @@
+// Level: easy
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        int count=1;
+        int preCount=0;
+        int ans=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                preCount=count;
+                count=1;
+            }
+            ans=max(ans,min(preCount,count));
+            ans = max(ans,count/2);
+        }
+        return ans>=k;
+    }
+};
+// time complexity: O(n)
+// space complexity: O(1)
