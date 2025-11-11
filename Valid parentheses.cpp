14d05d5e3129c34a212dc1983337f74c4ba6172d
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-11-11 23:10:15 +0530

    Create Valid parentheses.cpp

---
+++ Valid parentheses.cpp
@@ -0,0 +1,35 @@
+// Level: easy
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        for(auto it:s)
+        {
+            if(it=='(' || it=='{' || it=='[')
+            {
+                st.push(it);
+            }
+            else
+            {
+                if(st.size()==0)
+                {
+                    return false;
+                }
+                char ch = st.top();
+                if((ch=='(' && it==')') || (ch=='[' && it==']') || (ch=='{' && it=='}'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return st.size()==0;
+    }
+};
+
+// time complexity: O(n)
+// space complexity: O(n)
