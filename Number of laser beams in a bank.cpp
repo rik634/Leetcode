57bb0a8d8e1e012926b0eed7f869127b0a5813d8
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-11-25 23:33:47 +0530

    Update Number of laser beams in a bank.cpp

--- Number of laser beams in a bank.cpp
+++ Number of laser beams in a bank.cpp
@@ -30,3 +30,37 @@ public:
 };
 // Time complexity: O(n*m) + O(n)
 // Space complexity: O(n)
+
+
+// space optimized
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        
+        int n = bank.size();
+        
+        int prev=0;
+        int count=0;
+        int curr=0;
+        for(int i=0;i<n;i++)
+        {
+            curr=0;
+            for(auto it:bank[i])
+            {
+                if(it=='1')
+                {
+                    curr++;
+                }
+            }
+
+            if(curr!=0)
+            {
+                count+=(prev*curr);
+                prev=curr;
+            }
+        }
+        return count;
+    }
+};
+// Time complexity: O(n*m)
+// space complexity: O(1)
