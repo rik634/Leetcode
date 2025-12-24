2344be1c86e11fc72f0c031f0034558e83b34402
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-24 13:44:05 +0530

    Create Apple redistribution into boxes.cpp

---
+++ Apple redistribution into boxes.cpp
@@ -0,0 +1,42 @@
+// level: Easy
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        
+        int total = accumulate(apple.begin(),apple.end(),0);
+        priority_queue<int> pq(capacity.begin(),capacity.end());
+
+        int count=0;
+        while(total>0 && !pq.empty())
+        {
+            auto p = pq.top();
+            pq.pop();
+            total-=min(total,p);
+            count++;
+        }
+        return count;
+    }
+};
+// time complexity: O(n) + O(m)  = O(n+m)
+// space complexity: O(m)
+
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        
+        int total = accumulate(apple.begin(),apple.end(),0);
+       sort(capacity.begin(),capacity.end(),greater<int>());
+
+        int count=0;
+        int ind=0;
+        while(total>0 )
+        {
+            
+            total-=min(total,capacity[count]);
+            count++;
+        }
+        return count;
+    }
+};
+// time complexity: O(n+mlogm)
+// space complexity: O(m)
