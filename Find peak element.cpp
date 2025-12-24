48e6011d2bbc5e1c36634b0f7c0dabe29b173155
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-24 15:13:40 +0530

    Update Find peak element.cpp

--- Find peak element.cpp
+++ Find peak element.cpp
@@ -72,5 +72,27 @@ public:
 
     }
 };
+// OR
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+         int low = 0, high = nums.size() - 1;
+
+        
+        while (low < high) {
+           
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                
+                high = mid;
+            } else {
+                
+                low = mid + 1;
+            }
+        }
+        return low;
+
+    }
+};
 // Time complexity: O(logn)
 // space complexity: O(1)
