0398292225a345a691918512d834e32785727b0f
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-25 10:59:54 +0530

    Create Capacity to ship packages within d days.cpp

---
+++ Capacity to ship packages within d days.cpp
@@ -0,0 +1,110 @@
+// level: Medium
+// brute force
+class Solution {
+public:
+    int countDays(vector<int>& weights, int c)
+    {
+        int count=0;
+        int temp=0;
+        for(int j=0;j<weights.size();j++)
+        {
+            if(weights[j]>c)
+            {
+                return INT_MAX;
+            }
+            if((temp+weights[j])<=c)
+            {
+                temp+=weights[j];
+            }
+            else
+            {
+                count++;
+                temp=weights[j];
+            }
+        }
+        if(temp<=c)
+        {
+            count++;
+        }
+        return count;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int n = weights.size();
+        int mx = accumulate(weights.begin(),weights.end(),0);
+
+        for(int i=1;i<=mx;i++)
+        {
+            int d = countDays(weights,i);
+            if(d<=days)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+// Time complexity: O(mx*n)
+// space complexity: O(1)
+
+// binary search
+class Solution {
+public:
+     int countDays(vector<int>& weights, int c)
+    {
+        int count=0;
+        int temp=0;
+        for(int j=0;j<weights.size();j++)
+        {
+            if(weights[j]>c)
+            {
+                return INT_MAX;
+            }
+            if((temp+weights[j])<=c)
+            {
+                temp+=weights[j];
+            }
+            else
+            {
+                count++;
+                temp=weights[j];
+            }
+        }
+        if(temp<=c)
+        {
+            count++;
+        }
+        return count;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int n = weights.size();
+        int mx=0;
+        int mn = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            mx+=weights[i];
+            mn=min(mn,weights[i]);
+        }
+        int l=mn;
+        int r=mx;
+         int ans=mx;
+        while(l<=r)
+        {
+            int mid = (l+r)/2;
+            int count=countDays(weights,mid);
+            if(count<=days)
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+// Time complexity: O(n*logmx) + O(n) = O(n*(logmx+1))
+// space complexity: O(1)
