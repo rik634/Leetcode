6d645ff73b5a81f0fe14970cf8abb4cc232a3955
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-30 10:56:32 +0530

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,6 +1,7 @@
 // Level: Medium
-// Link: https://leetcode.com/problems/subsets/
-// Solution
+// problem statement:  Given a string, find all the possible subsequences of the string.
+// brute force
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -25,3 +26,28 @@ public:
 };
 // Time Complexity: O(n*(2^n)) (nested for loops)
 // Space Complexity: O(n*(2^n)) (there are total (2^n) subsets, in worst case each subset can have n elements)
+
+// optimized
+class Solution {
+public:
+    void getSubset(int ind, int& n, vector<int> v, vector<vector<int>>& res,vector<int>& nums)
+    {
+        if(ind==n)
+        {
+            res.push_back(v);
+            return;
+        }
+        getSubset(ind+1,n,v,res,nums);
+        v.push_back(nums[ind]);
+        getSubset(ind+1,n,v,res,nums);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<vector<int>> res;
+        getSubset(0,n,{},res,nums);
+        return res;
+    }
+};
+// time complexity: O(n*(2^n))
+// space complexity:O(n*(2^n))
