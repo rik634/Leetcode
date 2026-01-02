dd1053f7169a18026764cee788c8b7ddeafdeb5e
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-02 18:53:28 +0530

    Update Single Number.cpp

--- Single Number.cpp
+++ Single Number.cpp
@@ -14,3 +14,28 @@ public:
 };
 // Time Complexity: O(N) (N = Number of elements in array)
 // Space Complexity: O(1)
+
+// if the array is already sorted, this can be optimized further
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 2;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            // Check if mid is the first element of a pair
+            // (mid ^ 1) trick: 
+            // If mid is even, mid^1 is mid+1. 
+            // If mid is odd, mid^1 is mid-1.
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1; // We are in the left half, move right
+            } else {
+                high = mid - 1; // We are in the right half, move left
+            }
+        }
+        return nums[low];
+    }
+};
+// time complexity: O(logn)
+// space complexity:O(1)
