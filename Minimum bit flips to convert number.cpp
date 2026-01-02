4532ce266e9fb5d1d77921cb81b7808d407b7889
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-02 18:41:31 +0530

    Update Minimum bit flips to convert number.cpp

--- Minimum bit flips to convert number.cpp
+++ Minimum bit flips to convert number.cpp
@@ -24,3 +24,49 @@ public:
 };
 // Time Complexity: O(log (start^goal)) (base is 2)
 // Space Complexity: O(1)
+
+// optimal
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        bitset<32> b(start^goal); // 10 is 1010 in binary
+int count = b.count();
+return count;
+    }
+};
+// time complexity: o(1)
+// space complexity:O(1)
+
+// another optimal
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        
+        int count =__builtin_popcount(start^goal);
+        return count;
+    }
+};
+// time complexity: o(1)
+// space complexity:O(1)
+
+// another
+class Solution {
+public:
+  
+    int minBitsFlip(int start, int goal) {
+      
+        int num = start ^ goal;
+        int count = 0;
+
+        for(int i = 0; i < 32; i++) {
+
+            count += (num & 1); 
+            num = num >> 1;
+        }
+        return count;
+    }
+};
+// time complexity: o(1)
+// space complexity:O(1)
