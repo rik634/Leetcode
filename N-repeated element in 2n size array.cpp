20d67e58f155fba1d7842437bece1aa6344e3877
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-02 15:55:21 +0530

    Create N-repeated element in 2n size array.cpp

---
+++ N-repeated element in 2n size array.cpp
@@ -0,0 +1,22 @@
+// level: Easy
+
+// brute force
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        
+        unordered_set<int> st;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(st.find(nums[i])!=st.end())
+            {
+                return nums[i];
+            }
+            st.insert(nums[i]);
+        }
+        return 0;
+    }
+};
+// time complexity:O(2*n)
+// space complexity:O(n)
+
