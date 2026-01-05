eda7a385b35a1645626029c33f167604e38ded70
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-05 20:06:01 +0530

    Update Rotten Oranges.cpp

--- Rotten Oranges.cpp
+++ Rotten Oranges.cpp
@@ -1,5 +1,7 @@
 // level: Medium
-// problem statement: 
+// problem statement: Given an n x m grid, where each cell has the following values : 2 - represents a rotten orange , 1 - represents a Fresh orange , 0 - represents an Empty Cell .
+//Every minute, if a fresh orange is adjacent to a rotten orange in 4-direction ( upward, downwards, right, and left ) it becomes rotten.
+//Return the minimum number of minutes required such that none of the cells has a Fresh Orange. If it's not possible, return -1..
 
 // solution (BFS)
 class Solution {
