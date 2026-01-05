e3c48cef28dac4dc626631fd94832d0362b6ecce
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-05 19:01:36 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -31,5 +31,5 @@ class Solution {
         return bfs; 
     }
 };
-// time complexity: Time Complexity: O(N) + O(2E), Where N = Nodes, 2E is for total degrees as we traverse all adjacent nodes.
+// time complexity: Time Complexity: O(N+E), Where N = Nodes, 2E is for total degrees as we traverse all adjacent nodes.
 //Space Complexity: O(3N) ~ O(N), Space for queue data structure visited array and an adjacency list
