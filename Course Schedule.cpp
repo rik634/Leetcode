cf4a43140dd587489e1b7a193c3574d610e13de4
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-06 15:40:09 +0530

    Create Course Schedule.cpp

---
+++ Course Schedule.cpp
@@ -0,0 +1,88 @@
+// level: Medium
+// problem statement: 
+
+// BFS;
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        int n = numCourses;
+        vector<int> indegree(n, 0);
+        vector<int> adj[n];
+        queue<int> q;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            indegree[prerequisites[i][0]]++;
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        int count = 0;
+        while (!q.empty()) {
+            auto p = q.front();
+            q.pop();
+            count++;
+            for (auto it : adj[p]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return count == n;
+    }
+};
+
+// DFS:
+class Solution {
+public:
+    bool dfs(int node, vector<bool>& vis, vector<bool>& pathVis, vector<int> adj[])
+    {
+        vis[node]=true;
+        pathVis[node]=true;
+        for(auto it:adj[node])
+        {
+            if(vis[it]==false)
+            {
+                if(dfs(it,vis,pathVis,adj)==true)
+                {
+                    return true;
+                }
+            }
+            else if(pathVis[it]==true)
+            {
+                return true;
+            }
+        }
+        pathVis[node]=false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+
+        int n = numCourses;
+        vector<int> adj[n];
+        vector<bool> vis(n,false);
+        vector<bool> pathVis(n,false);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==false)
+            {
+                if(dfs(i,vis,pathVis,adj))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+// Time complexity: O(v+E) (build adj list) + O(V) + O(E) (dfs) = O(V+E)
+// space complexity: O(V+E) (adj list) + O(V) (vis) + O(V) (recursive stack space) = O(V+E)
