88246f22360c6e67296ee4a30ae786ebe5fb7a27
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-08 17:52:16 +0530

    Update Number of ways to arrive at destination.cpp

--- Number of ways to arrive at destination.cpp
+++ Number of ways to arrive at destination.cpp
@@ -1,58 +1,167 @@
 //level:medium
 //link: https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/
-//solution
+// problem statement:
 
+
+// brute force (array)
 class Solution {
 public:
-    const long long M = 1000000007;
-    unordered_map<int,int> mp;
-    
-    int dfs(int& n, vector<vector<pair<int,int>>>& adj)
-    {
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,greater<>> pq;
-        vector<long long> dist(n,LLONG_MAX);
-        dist[0]=0;
-        mp.insert({0,1});
-        pq.push({0,0});
-        while(!pq.empty())
-        {
-            auto p = pq.top();
-            pq.pop();
-            int node = p.second;
-            long long time = p.first;
-            if(time > dist[node])
-            {
-                continue;
-            }
-            for(auto& it:adj[node])
-            {
-                if(dist[it.first]>(time+it.second))
-                {
-                    dist[it.first]=time + it.second;
-                    mp[it.first]=mp[node];
-                    pq.push({dist[it.first],it.first});
-                }
-                else if(dist[it.first]==time + it.second)
-                {
-                    mp[it.first]=(mp[it.first]+mp[node])%M;
-                }
-            }
-        }
-        return mp[n-1];
-    }
     int countPaths(int n, vector<vector<int>>& roads) {
 
-        vector<vector<pair<int,int>>> adj(n);
+        const long long MOD = (long long)1e9 + 7;
+        vector<pair<int, int>> adj[n];
+        for (int i = 0; i < roads.size(); i++) {
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int time = roads[i][2];
+            adj[u].push_back({v, time});
+            adj[v].push_back({u, time});
+        }
+        vector<long long> time(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+        vector<bool> vis(n,false);
+        time[0] = 0;
+        ways[0] = 1;
+        for(int i=0;i<n;i++){
+            int u =-1;
+            for(int v=0;v<n;v++){
+                if(vis[v]==false && (u==-1 || time[v]<time[u])){
+                    u=v;
+                }
+            }
+
+            if(time[u]==LLONG_MAX) {
+                break;
+            }
+            for(auto it:adj[u]) {
+                long long t = it.second;
+                long long v = it.first;
+                if((time[u]+t)<time[v]) {
+                    time[v]=time[u]+t;
+                    ways[v]=ways[u];
+                }else if(time[u]+t==time[v]){
+                    ways[v]=(ways[v]+ways[u])%MOD;
+                }
+            }
+            vis[u]=true;
+        }
+        return ways[n-1]%MOD;
+    }
+};
+// Time complexity: O(V*V+E)
+// space complexity: O(V+E)
+
+// further optimized (using queue)
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        const long long MOD=(long long)1e9+7;
+        vector<pair<int,int>> adj[n];
         for(int i=0;i<roads.size();i++)
         {
             int u = roads[i][0];
             int v = roads[i][1];
-            int t = roads[i][2];
-            adj[u].push_back({v,t});
-            adj[v].push_back({u,t});
+            int time = roads[i][2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
         }
-        int d =n-1;
-        return dfs(n,adj);
-       
+        vector<long long> time(n,LLONG_MAX);
+        vector<int> ways(n,0);
+        time[0]=0;
+        ways[0]=1;
+        priority_queue<pair<long,long>,vector<pair<long,long>>,greater<pair<long,long>>> q;
+        q.push({time[0],0});
+        while(!q.empty())
+        {
+            auto p = q.top();
+            q.pop();
+            long long t = p.first;
+            long long u = p.second;
+            if(t>time[u])
+            {
+                continue;
+            }
+            for(auto it:adj[u])
+            {
+                long long tt = it.second;
+                long long v = it.first;
+                if((t+tt)<time[v])
+                {
+                    time[v]=time[u]+tt;
+                    ways[v]=ways[u];
+                    q.push({time[v],v});
+                }
+                else if(time[v]==(time[u]+tt))
+                {
+                    ways[v]=(ways[v]+ways[u])%MOD;
+                }
+            }
+        }
+        return ways[n-1]%MOD;
+
     }
 };
+// Time complexity: O(ElogV)
+// space complexity: O(V+E)
+
+
+
+// optimized (using set)
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        
+        const long long MOD=(long long)1e9+7;
+        vector<pair<int,int>> adj[n];
+        for(int i=0;i<roads.size();i++)
+        {
+            int u = roads[i][0];
+            int v = roads[i][1];
+            int time = roads[i][2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
+        }
+        vector<long long> time(n,LLONG_MAX);
+        vector<int> ways(n,0);
+        time[0]=0;
+        ways[0]=1;
+       
+        set<pair<long long,long long>> q;
+        q.insert({time[0],0});
+        while(!q.empty())
+        {
+            
+            long long t = q.begin()->first;
+            long long u = q.begin()->second;
+            q.erase(q.begin());
+            if(t>time[u])
+            {
+                continue;
+            }
+            for(auto it:adj[u])
+            {
+                long long tt = it.second;
+                long long v = it.first;
+                if((t+tt)<time[v])
+                {
+                    if(time[v]!=LLONG_MAX)
+                    {
+                        q.erase({time[v],v});
+                    }
+                    time[v]=time[u]+tt;
+                    ways[v]=ways[u];
+                    q.insert({time[v],v});
+                }
+                else if(time[v]==(time[u]+tt))
+                {
+                    ways[v]=(ways[v]+ways[u])%MOD;
+                }
+            }
+        }
+        return ways[n-1]%MOD;
+
+    }
+};
+// Time complexity: O(ElogV)
+// space complexity: O(E+V)
