c44455d0effa44cabecdd2f2b830ab3cc86b0826
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2026-01-12 15:49:58 +0530

    Update Articulation point.cpp

--- Articulation point.cpp
+++ Articulation point.cpp
@@ -2,74 +2,61 @@
 // Link:
 // Solution
 // User function Template for C++
-
 class Solution {
 private:
-    int timer=0;
-private:
+    int timer = 1;
 
-    void dfs(int node, int parent, vector<bool>& vis, vector<int> adj[], vector<int>& tin, vector<int>& low, vector<int>& mark)
-    {
-        vis[node]=true;
-        tin[node]=low[node]=timer;
-        timer++;
-        int child=0;
-        for(auto it: adj[node])
-        {
-            if(it==parent)
-            {
-                continue;
-            }
-            if(vis[it]==false)
-            {
-                dfs(it,node,vis,adj,tin,low,mark);
-                low[node]=min(low[node],low[it]);
-                if(low[it]>=tin[node] && parent!=-1)
-                {
-                    mark[node]=1;
+    // DFS to find articulation points
+    void dfs(int node, int parent, vector<int> &vis, int tin[], int low[],
+             vector<int> &mark, vector<int> adj[]) {
+
+        vis[node] = 1;
+        tin[node] = low[node] = timer++; // set discovery and low time
+        int child = 0;
+
+        for (auto it : adj[node]) {
+            if (it == parent) continue; // skip the edge to parent
+
+            if (!vis[it]) {
+                dfs(it, node, vis, tin, low, mark, adj); // recursive DFS
+                low[node] = min(low[node], low[it]);     // update low time
+
+                // Check articulation condition (excluding root)
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = 1;
                 }
                 child++;
             }
-            else
-            {
-                low[node]=min(low[node],tin[it]);
+            else {
+                // back edge case
+                low[node] = min(low[node], tin[it]);
             }
-            
         }
-        if(child>1 && parent==-1)
-        {
-            mark[node]=1;
+
+        // If root node has more than one child
+        if (parent == -1 && child > 1) {
+            mark[node] = 1;
         }
-        
     }
+
 public:
-    vector<int> articulationPoints(int V, vector<int> adj[]) {
-        // Code here
-        vector<bool> vis(V,false);
-        vector<int> tin(V,0);
-        vector<int> low(V,0);
-        vector<int> mark(V,0);
-        for(int i=0;i<V;i++)
-        {
-            if(vis[i]==false)
-            {
-                dfs(i,-1,vis,adj,tin,low,mark);
+    vector<int> articulationPoints(int n, vector<int> adj[]) {
+        vector<int> vis(n, 0), mark(n, 0);
+        int tin[n], low[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(i, -1, vis, tin, low, mark, adj);
             }
         }
+
         vector<int> ans;
-        for(int i=0;i<V;i++)
-        {
-            if(mark[i]==1)
-            {
-                ans.push_back(i);
-            }
+        for (int i = 0; i < n; i++) {
+            if (mark[i]) ans.push_back(i);
         }
-        if(ans.size()==0)
-        {
-            return {-1};
-        }
-        return ans;
+        return ans.empty() ? vector<int>{-1} : ans;
     }
 };
-// Time Complexity: O(V+2E) (DFS)
-// Space Complexity: O(3V)
+
+// Time Complexity: O(V+2E), where V = no. of vertices, E = no. of edges. It is because the algorithm is just a simple DFS traversal.
+//Space Complexity: O(3V), where V = no. of vertices. O(3V) is for the three arrays i.e. tin, low, and vis, each of size V.
